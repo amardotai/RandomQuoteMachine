5b4f42d3d347f7fc0f4c2fce9868e00702302515
Amarnath Banerjee <amarnath.kbanerjee@gmail.com>
2023-01-28 21:16:14 +0530

    got final look and behaviour

--- quotes.js
+++ quotes.js
@@ -1,6 +1,5 @@
 
-
-const quote = [
+const quotesList = [
     "'To live is the rarest thing in the world. Most people exist, that is all.'",
     "'That it will never come again is what makes life so sweet.'",
     "'It is never too late to be what you might have been.'",
@@ -12,7 +11,8 @@ const quote = [
     "'Let me live, love, and say it well in good sentences.'",
     "'Don't let your happiness depend on something you may lose.' "
 ];
-const author = [
+
+let authorList = [
     "Oscar Wilde",
     "Emily Dickinson",
     "George Eliot",
@@ -25,25 +25,4 @@ const author = [
     "C.S. Lewis"
 ];
 
-const colors = [
-    "#3d7475","#6eb5aa","#819a78","#277e71","#2cccd3","#72e6cb","#a1cfd0","#daa06d","#f36a6f","#efa514"
-];
-
-genrateQuote = ()=>{
-    let newQuote = Math.floor(Math.random()*10);
-    $("#text").text(quote[newQuote]);
-    $("#author").text("- ".concat(author[newQuote]));
-    generateColor();
-}
-generateColor = ()=>{
-    let newColor = Math.floor(Math.random()*colors.length);
-    let r = document.querySelector(":root");
-    document.documentElement.style.setProperty('--theme',colors[newColor]);
-}
-genrateQuote();
-generateColor();
-
-
-
-
-
+export default {quotesList,authorList};
